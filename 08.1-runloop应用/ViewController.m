70e90a582af35e383012a72239a5f4a27e0bed23
刘光强 <1126756952@qq.com>
2020-06-21 17:59:19 +0800

    初始化代码仓库

--- 08.1-runloop应用/ViewController.m
+++ 08.1-runloop应用/ViewController.m
@@ -14,10 +14,28 @@
 
 @implementation ViewController
 
+void test1() {
+    static int num = 0;
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+         NSLog(@"---%d", num ++);
+     }];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+}
+
+void test2() {
+    static int num = 0;
+     NSTimer *timer = [NSTimer timerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+         NSLog(@"---%d", num ++);
+     }];
+     
+     [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+    test1();
+//    test2();
 }
-
-
 @end
